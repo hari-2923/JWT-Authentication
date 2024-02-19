5255b52be00608d8b953c268b334711ff2573121
Hari Govind <harigovind0221@gmail.com>
2024-02-19 11:14:16 +0300

    Refresh Tokens Added

--- server.js
+++ server.js
@@ -7,16 +7,43 @@ const jwt = require("jsonwebtoken")
 app.listen(process.env.PORT || 5000)
 app.use(bodyParser.json())
 
+let refreshTokens = [ ]
+
+app.post("/token", (req,res) => {
+    const refreshToken = req.body.token
+    console.log("Received refresh token:", refreshToken)
+    if (!refreshToken) {
+        console.log("No refresh token provided")
+        return res.sendStatus(401)
+    }
+    if(!refreshTokens.includes(refreshToken)){
+        return res.sendStatus(403)
+    }
+    else{
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403)
+            }
+            else{
+                const accessToken = generateAccessToken({ name: user.name })
+                console.log("Generated Token");
+                res.json({ accessToken: accessToken })
+            }
+        })
+    }
+})
+
 app.post("/users", (req,res) =>{
     const username = req.body.username
     const user = { name: username }
-    const acceessToken = generateAccessToken(user)
+    const accessToken = generateAccessToken(user)
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    res.json({ acceessToken: acceessToken, refreshToken: refreshToken })
+    refreshTokens.push(refreshToken)
+    res.json({ accessToken: accessToken, refreshToken: refreshToken })
     
 })
 
 // Middleware for token Authentication
 function generateAccessToken (user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "3s" })
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30s" })
 }
