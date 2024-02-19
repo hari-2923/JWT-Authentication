1f40bc9fc015c070a666197d0f6fc5be4ba31d20
Hari Govind <harigovind0221@gmail.com>
2024-02-19 10:35:10 +0300

    Added Refresh Tokens

--- app.js
+++ app.js
@@ -27,17 +27,21 @@ app.get("/users", authenticateToken, (req,res) => {
 
 // Middleware for token Authentication
 function authenticateToken(req,res,next) {
+    console.log("Authenticating token...");
     const authHeader = req.headers["authorization"]
     const token = authHeader && authHeader.split(" ")[1]
-    if(token === null){
+    if(token == null){
+        console.log("Token not found");
         res.sendStatus(401)
     }
     else{
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
             if(err){
+                console.error("Token verification failed:", err.message)
                 return res.sendStatus(403)
             }
             else{
+                console.log("Token verified successfully");
                 req.user = user
                 next()
             }
